09e6431caf6c3fd89bf55a8fd5f6cf9f4e84658e
Sunil Kumar C <sunil10790@outlook.com>
2020-10-11 14:40:22 +0530

    Server Side Rendering Initial Commit

---
+++ server/src/client/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <div>App COmponent with nodemon</div>
+      <button onClick={() => console.log('button click')}>Click me</button>
+    </div>
+  );
+};
+
+export default App;
